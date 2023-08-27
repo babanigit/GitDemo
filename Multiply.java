1e62085c88d4166152807fcf24640d099ea64e99
Aniket Panchal <aniketpanchal07@gmail.com>
2023-08-27 14:00:43 +0530

    added multiplybablu

---
+++ Multiply.java
@@ -0,0 +1,12 @@
+
+
+
+class Multiply{  
+    public static void main(String args[]){  
+
+        int a = 34;
+        int b = 34;
+     System.out.println("Hello Java");  
+     System.out.println("the sum" + (a*b));
+    }  
+}  

f71d06d741e293256b63e3629b420ca070b54a31
Aniket Panchal <aniketpanchal07@gmail.com>
2023-08-27 13:47:28 +0530

    adding message in sum

--- sum.java
+++ sum.java
@@ -7,6 +7,6 @@ class sum{
         int a = 34;
         int b = 34;
      System.out.println("Hello Java");  
-     System.out.println(a+b);
+     System.out.println("the sum" + (a+b));
     }  
 }  

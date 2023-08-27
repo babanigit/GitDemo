151c5d514a6a0e26d4a9c62aff241d4135256e69
Aniket Panchal <aniketpanchal07@gmail.com>
2023-08-27 12:44:19 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,12 @@
+
+
+
+class sum{  
+    public static void main(String args[]){  
+
+        int a = 34;
+        int b = 34;
+     System.out.println("Hello Java");  
+     System.out.println(a+b);
+    }  
+}  
